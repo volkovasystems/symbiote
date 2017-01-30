bf95d2f8b980c5be083e04e7f3c78118d90a4d01
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-30 12:50:19 +0800

    Add optimization.

---
+++ .install.sh
@@ -0,0 +1,19 @@
+yarn add \
+babel-runtime \
+harden \
+protype \
+raze \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+harden \
+protype \
+raze \
+--save
